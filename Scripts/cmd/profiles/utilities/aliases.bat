f5358e38e15b8c2d77cec45144f60fd065626899
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-11-16 16:50:02 +0000

    update - add alias utility to create new commands from cli

--- Scripts/cmd/profiles/utilities/aliases.bat
+++ Scripts/cmd/profiles/utilities/aliases.bat
@@ -5,3 +5,9 @@
   echo echo. >> %~dp0\%1.bat
   echo %ALL_BUT_FIRST% %%* >> %~dp0\%1.bat
   echo Created alias for %1=%ALL_BUT_FIRST%
+
+REM This bat script will create an alias for the command you specify.
+REM For example, if you want to create an alias for the command "dir", you would type:
+REM   alias dir
+REM or another example, if you want to create an alias for the command "dir /s", you would type:
+REM   alias dir /s
